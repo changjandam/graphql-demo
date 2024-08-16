4c55b8f84ed8f88e191071190c5b9c4251f88bfc
Jimmy Hu <jimmy@jgallop.com>
2024-08-16 15:58:19 +0800

    init server

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {},
+});
+
+startStandaloneServer(server);
