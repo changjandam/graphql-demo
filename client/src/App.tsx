8028a6983d4ec92df2f785b629a8e49e26b55262
Jimmy Hu <jimmyhu.coding@gmail.com>
2024-08-18 22:05:34 +0800

    feat: add client side query demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import { Router } from './router';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <></>;
+  return (
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
+  );
 }
 
 export default App;
